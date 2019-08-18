962629fd4a05df741676ef3cecacd746b5e2dc65
Terry Zhang <tz@terryzhang.com>
2019-08-19 02:10:31 +0800

    fix local storage

--- localstorage.js
+++ localstorage.js
@@ -1,23 +1,4 @@
 class Storage {
-  getItemAsync(key) {
-    console.log("GET ITEM ASYNC!", key)
-    return new Promise((resolve, reject) => {
-      Taro.getStorage({ key, success: res => resolve(res), fail: err => reject(err) })
-    })
-  }
-
-  setItemAsync(key, value) {
-    console.log("SET ITEM ASYNC!", key, value)
-    return new Promise((resolve, reject) => {
-      Taro.setStorage({
-        key: key,
-        data: value,
-        success: res => resolve(res),
-        fail: err => reject(err)
-      })
-    })
-  }
-
   getItem(key) {
     return Taro.getStorageSync(key)
   }
